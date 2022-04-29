c00ca8f5ad26105b6ea41c2b8ce8c16067de9f35
Maikel De Werd <maikeldewerd@hotmail.com>
2022-04-29 09:53:08 +0200

    test5

--- Swipe.js
+++ Swipe.js
@@ -25,7 +25,7 @@ function moveBlok(event){
   let delta_x=event.movementX;
   let delta_y=event.movementY; 
   let element=event.target;
-  console.log(event.movementX, event.movementY);
+  console.log(event);
   //nieuwe locatie aangeven waarnaar het plaatje beweegt
   let location=element.getBoundingClientRect();  
   console.log((location.left+delta_x),(location.left+delta_y));
